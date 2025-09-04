93312930781e2010dc807e0972a813de885b6d11
Souleymane Diallo <sdley2020@gmail.com>
2025-09-04 12:30:04 +0000

    Data Binding from parent to child

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GreetingComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  homeMessage = signal('Hello from Home Component!');
 }
