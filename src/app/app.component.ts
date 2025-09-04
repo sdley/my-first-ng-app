048de408cbc75868a7c57bae9a57b45f77025145
Souleymane Diallo <sdley2020@gmail.com>
2025-09-04 16:34:30 +0000

    Creating routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <app-header />
-    <main>
-      <app-home />
-    </main>
+    <main></main>
     <router-outlet />
   `,
   styles: [
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  titlePart1 = signal('Welcome to My First Angular App');
-  titlePart2 = ' - A Journey into Angular';
+  titlePart1 = signal('My First Angular');
+  titlePart2 = ' App';
 }
