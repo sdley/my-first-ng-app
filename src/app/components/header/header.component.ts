d2fe34b042279baf93450aa55aa1ed5c6ad1b5c4
Souleymane Diallo <sdley2020@gmail.com>
2025-09-04 10:42:09 +0000

    Data Binding

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  titlePart1 = signal('Welcome to My First Angular App');
+  titlePart2 = ' - A Journey into Angular';
 }
