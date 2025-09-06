11cbc4fa875208cb81451e6d1e8528b42b14f78b
Souleymane Diallo <sdley2020@gmail.com>
2025-09-06 12:15:26 +0000

    Angular Directives

---
+++ src/app/directives/highlight-completed-todo.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, effect, ElementRef, inject, input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightCompletedTodo]',
+})
+export class HighlightCompletedTodoDirective {
+  isCompleted = input(false);
+  el = inject(ElementRef);
+  stylesEffect = effect(() => {
+    if (this.isCompleted()) {
+      this.el.nativeElement.style.textDecoration = 'line-through';
+      this.el.nativeElement.style.color = 'gray';
+      this.el.nativeElement.style.backgroundColor = '#d3f9d8';
+    } else {
+      this.el.nativeElement.style.textDecoration = 'none';
+      this.el.nativeElement.style.color = 'black';
+      this.el.nativeElement.style.backgroundColor = 'transparent';
+    }
+  });
+
+  constructor() {}
+}
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { NgIf } from '@angular/common';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
@@ -26,5 +28,19 @@ export class TodosComponent implements OnInit {
         next: (todos) => this.todoItems.set(todos),
         error: (err) => console.error('Error fetching todos:', err),
       });
+  }
+
+  updateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if (todo.id === todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+    });
   }
 }
